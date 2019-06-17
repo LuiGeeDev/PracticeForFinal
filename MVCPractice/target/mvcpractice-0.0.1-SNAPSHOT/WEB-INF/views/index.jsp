27f0d07269174756f401ae43e2db017973911cd4
LuiGee3471 <yjs3475@gmail.com>
2019-06-17 20:53:17 +0900

    MVCTemplate

---
+++ MVCPractice/target/mvcpractice-0.0.1-SNAPSHOT/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: USER
+  Date: 2019-06-14
+  Time: 오후 8:38
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Title</title>
+</head>
+<body>
+  INDEX PAGE
+  <a href="hello">Go to Hello</a>
+</body>
+</html>
---
+++ MVCTemplate/MVCtemplate/src/main/java/kr/or/bit/model/Board.java
@@ -0,0 +1,40 @@
+package kr.or.bit.model;
+
+public class Board {
+  private int id;
+  private String title;
+  private String content;
+  private String writer;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public String getWriter() {
+    return writer;
+  }
+
+  public void setWriter(String writer) {
+    this.writer = writer;
+  }
+}
